3534168def92715b4089a3527083b92cd843eb4e
Raman yadav <40426625+RamanDonald@users.noreply.github.com>
2023-02-05 10:23:26 +0530

    import java.util.*; class s {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         char r=sc.next().charAt(0);         int f=r;         System.out.print(f);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class s
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        char r=sc.next().charAt(0);
+        int f=r;
+        System.out.print(f);
+    }
+}
