e627fdedaa2adb171a13f115d398aa41af9c462b
Raman yadav <40426625+RamanDonald@users.noreply.github.com>
2023-02-05 11:01:00 +0530

    import java.util.*;   class s   {       public static void main(String[] args)       {           Scanner sc=new Scanner(System.in);            String r=sc.nextLine();             System.out.print(r);       }   }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+  import java.util.*;
+  class s
+  {
+      public static void main(String[] args)
+      {
+          Scanner sc=new Scanner(System.in);
+           String r=sc.nextLine();
+            System.out.print(r);
+      }
+  }
+  
