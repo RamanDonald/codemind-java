2f5e05eaab878f331f95aaa155f4de646d1f78d6
Raman yadav <40426625+RamanDonald@users.noreply.github.com>
2023-02-05 11:06:11 +0530

    import java.util.*;   class s   {       public static void main(String[] args)       {           Scanner sc=new Scanner(System.in);             long a=sc.nextLong();             long b=sc.nextLong();             long c=sc.nextLong();             long avg=(a*b*c*512*2);             System.out.print(avg);       }   }

---
+++ Disk_Capacity.java
@@ -0,0 +1,14 @@
+  import java.util.*;
+  class s
+  {
+      public static void main(String[] args)
+      {
+          Scanner sc=new Scanner(System.in);
+            long a=sc.nextLong();
+            long b=sc.nextLong();
+            long c=sc.nextLong();
+            long avg=(a*b*c*512*2);
+            System.out.print(avg);
+      }
+  }
+  
