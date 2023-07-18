6754286a6cf90f7eca55e896fbfd4dbf07848e0c
Raman yadav <40426625+RamanDonald@users.noreply.github.com>
2023-07-18 20:42:03 +0530

    import java.util.*; public class AdamNum{     public static int reverse(int n){           int rev=0;          while(n>0){             int rem=n%10;             rev=rev*10+rem;             n=n/10;         }         return rev;     }     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.println(reverse(n));     } }

---
+++ reverse_of_a_numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AdamNum{
+    public static int reverse(int n){
+          int rev=0;
+         while(n>0){
+            int rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+        return rev;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(reverse(n));
+    }
+}
